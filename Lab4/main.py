86358c9da7a80df57051f49537617d2a32cfb1c8
Dadmon <maksklym-ipt27@lll.kpi.ua>
2025-12-13 17:08:25 +0200

    Lab 4 started

---
+++ Lab4/main.py
@@ -0,0 +1,80 @@
+import time
+import subprocess
+from multiprocessing import Process, Pool
+from pymongo import MongoClient, WriteConcern
+
+URI = "mongodb://mongo1:27017,mongo2:27017,mongo3:27017/?replicaSet=replicaset"
+client = MongoClient(URI)
+
+DB = "testdb"
+COL = "likes"
+DOC_ID = "post1"
+CLIENTS = 10
+INCREMENTS = 10_000
+col = client[DB][COL]
+
+def sh(cmd):
+    subprocess.run(cmd, shell=True, check=False)
+
+def prepare_db():
+    # client = MongoClient(URI)
+    # col = client[DB][COL]
+    col.delete_many({})
+    col.insert_one({"_id": DOC_ID, "likes": 0})
+    # client.close()
+
+def worker(write_concern):
+    col = client[DB].get_collection(COL, write_concern=write_concern)
+    for _ in range(INCREMENTS):
+        col.find_one_and_update({"_id": DOC_ID}, {"$inc": {"likes": 1}})
+
+
+
+# def worker(write_concern):
+#     client = MongoClient(URI)
+#     col = client[DB].get_collection(COL, write_concern=write_concern)
+#     for _ in range(INCREMENTS):
+#         col.find_one_and_update({"_id": DOC_ID}, {"$inc": {"likes": 1}})
+#     client.close()
+
+
+def run_test(write_concern, kill_primary=False):
+    print(f"[TEST] writeConcern = {write_concern.document}")
+    prepare_db()
+
+    procs = []
+    start = time.time()
+
+    with Pool(CLIENTS) as p:
+        p.map(worker, [write_concern]*CLIENTS)
+
+    # for _ in range(CLIENTS):
+    #     p = Process(target=worker, args=(write_concern,))
+    #     p.start()
+    #     procs.append(p)
+
+    if kill_primary:
+        time.sleep(2)
+        print(f"[KILL] Stopping PRIMARY (mongo1)")
+        sh("docker stop mongo1")
+        time.sleep(8)
+        sh("docker start mongo1")
+
+    # for p in procs:
+    #     p.join()
+
+    elapsed = time.time() - start
+
+    # client = MongoClient(URI)
+    result = client[DB][COL].find_one({"_id": DOC_ID})
+    client.close()
+
+    print(f"[RESULTS] Time: {elapsed:.2f} sec | likes = {result['likes']}")
+
+
+if __name__ == "__main__":
+    run_test(WriteConcern(w=1))
+    # run_test(WriteConcern(w="majority"))
+
+    # run_test(WriteConcern(w=1), kill_primary=True)
+    # run_test(WriteConcern(w="majority"), kill_primary=True)
