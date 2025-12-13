55e5326eb0dc031d9e809f717e721e513fff1570
Dadmon <maksklym-ipt27@lll.kpi.ua>
2025-12-13 17:30:36 +0200

    Lab 4 started

--- Lab4/main.py
+++ Lab4/main.py
@@ -3,31 +3,6 @@ import subprocess
 from multiprocessing import Process, Pool
 from pymongo import MongoClient, WriteConcern
 
-URI = "mongodb://mongo1:27017,mongo2:27017,mongo3:27017/?replicaSet=replicaset"
-client = MongoClient(URI)
-
-DB = "testdb"
-COL = "likes"
-DOC_ID = "post1"
-CLIENTS = 10
-INCREMENTS = 10_000
-col = client[DB][COL]
-
-def sh(cmd):
-    subprocess.run(cmd, shell=True, check=False)
-
-def prepare_db():
-    # client = MongoClient(URI)
-    # col = client[DB][COL]
-    col.delete_many({})
-    col.insert_one({"_id": DOC_ID, "likes": 0})
-    # client.close()
-
-def worker(write_concern):
-    col = client[DB].get_collection(COL, write_concern=write_concern)
-    for _ in range(INCREMENTS):
-        col.find_one_and_update({"_id": DOC_ID}, {"$inc": {"likes": 1}})
-
 
 
 # def worker(write_concern):
@@ -38,17 +13,34 @@ def worker(write_concern):
 #     client.close()
 
 
-def run_test(write_concern, kill_primary=False):
+def run_test(write_concern, kill_primary=False, clients=10, iterations=10):
+    URI = "mongodb://mongo1:27017,mongo2:27017,mongo3:27017/?replicaSet=replicaset"
+    client = MongoClient(URI)
+    DB = "testdb"
+    COL = "likes"
+    DOC_ID = "post1"
+    col = client[DB][COL]
+    col.delete_many({})
+    col.insert_one({"_id": DOC_ID, "likes": 0})
+
+    def sh(cmd):
+        subprocess.run(cmd, shell=True, check=False)
+
+    def worker(write_concern):
+        col = client[DB].get_collection(COL, write_concern=write_concern)
+        for _ in range(iterations):
+            col.find_one_and_update({"_id": DOC_ID}, {"$inc": {"likes": 1}})
+
+
     print(f"[TEST] writeConcern = {write_concern.document}")
-    prepare_db()
 
     procs = []
     start = time.time()
 
-    with Pool(CLIENTS) as p:
-        p.map(worker, [write_concern]*CLIENTS)
+    with Pool(clients) as p:
+        p.map(worker, [write_concern]*clients)
 
-    # for _ in range(CLIENTS):
+    # for _ in range(clients):
     #     p = Process(target=worker, args=(write_concern,))
     #     p.start()
     #     procs.append(p)
@@ -76,5 +68,5 @@ if __name__ == "__main__":
     run_test(WriteConcern(w=1))
     # run_test(WriteConcern(w="majority"))
 
-    # run_test(WriteConcern(w=1), kill_primary=True)
+    run_test(WriteConcern(w=1), kill_primary=True)
     # run_test(WriteConcern(w="majority"), kill_primary=True)
